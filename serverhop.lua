9a424eb8584cc5652c4c9c3cea22287f1b4155f6
m.better <ppro07570@gmail.com>
2025-02-22 17:48:51 +0700

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,3 +1,4 @@
+wait(30)
 local Number = 0
 local SomeSRVS = {}
 for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
